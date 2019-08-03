6a0491ed0041fd963d61abe0c5163ad1133be8d9
Andrew Hill <andrewjrhill@gmail.com>
2019-08-03 17:24:44 +0200

    scans sort function

--- src/components/scans/scans-list-component/ScansList.js
+++ src/components/scans/scans-list-component/ScansList.js
@@ -5,14 +5,34 @@ import ScansListItem from '../scans-list-item-component/ScansListItem';
 class ScansList extends React.Component {
     scansLength = this.props.scans.length;
 
+    /**
+     * Sort scans by property and direction.
+     * @param { string } property
+     * Accepts "name", "elevation" or "username". Determines which property to sort scans by.
+     * @param { string } direction
+     * Accepts "asc" or "desc". Determines the direction in which to sort the scans.
+     * @param { array } scans
+     * Accepts an array of "scans" to sort.
+     */
+    sortBy(property, direction, scans) {
+        if (!property.includes('elevation')) {
+            return scans.sort((a, b) => direction === 'asc'
+                ? a[property].localeCompare(b[property])
+                : b[property].localeCompare(a[property]));
+        }
+
+        return scans.sort((a, b) => direction === 'asc'
+            ? a[property] - b[property]
+            : b[property] - a[property]);
+    }
+
     render() {
         return (
             <div className="ScansList">
                 <h1>Scans</h1>
                 <p className="results-length">Displaying 1 - { this.scansLength } of { this.scansLength } results.</p>
 
-
-                {this.props.scans.map(scan => (
+                {this.sortBy('name', 'asc', this.props.scans).map(scan => (
                     <ScansListItem
                         elevationMax={ scan.elevationMax }
                         elevationMin={ scan.elevationMin }
